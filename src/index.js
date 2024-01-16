c255a2cb0354e065ac7719c7558b660e4c1adbdc
kjderevianko <kathrin.derevianko@gmail.com>
2024-01-16 13:01:38 +0100

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -10,12 +10,15 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
-  let instructionsInput = document.queryCommandValue("#user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "797fa140830ac961d4b9d418btd23b6o";
   let prompt = `User instructions: Generate a poem in Shakespeare language about ${instructionsInput.value}`;
   let context =
     "You are a romentic poem expert and ove to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Don't include any title to the poem. Make sure to follow the user instructions.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink"> ⌛️ Generating a poem about ${instructionsInput.value}</div>`;
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
