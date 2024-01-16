2c2e93511f3feac7ec52cc53c22824c226e93d43
kjderevianko <kathrin.derevianko@gmail.com>
2024-01-16 16:42:52 +0100

    Modified CSS for the text input

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function generatePoem(event) {
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="blink"> ⌛️ Generating a poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="blink"> Generating a poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
